9baf6556513c65c01dbf83f7bf66d54d9bd96c90
Jair <smtr139023@ugb.edu.sv>
2024-02-22 22:58:27 -0600

    nueva rama

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -2,184 +2,63 @@ package com.ugb.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TabHost tbh;
     TextView tempVal;
-    Spinner spn;
-    Button btnConvertirLongitud;
-    Button btnConvertirAlmacenamiento;
-    Button btnConvertirMonedas;
-    Button btnConvertirMasa;
-    Button btnConvertirVolumen;
-    Button btnConvertirTiempo;
-    Button btnConvertirTransferenciaDeDatos;
-    conversores miObj = new conversores();
-
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tbh = findViewById(R.id.tbhConversor);
-        tbh.setup();
-
-        tbh.addTab(tbh.newTabSpec("LON").setContent(R.id.tab1Longitud).setIndicator("LONGITUD", null));
-        tbh.addTab(tbh.newTabSpec("ALM").setContent(R.id.tab2Almacenamiento).setIndicator("ALMACENAMIENTO", null));
-        tbh.addTab(tbh.newTabSpec("MON").setContent(R.id.tab3Monedas).setIndicator("MONEDAS", null));
-        tbh.addTab(tbh.newTabSpec("MAS").setContent(R.id.tab4Masa).setIndicator("MASA", null));
-        tbh.addTab(tbh.newTabSpec("VOL").setContent(R.id.tab5Volumen).setIndicator("VOLUMEN", null));
-        tbh.addTab(tbh.newTabSpec("TIE").setContent(R.id.tab6Tiempo).setIndicator("TIEMPO", null));
-        tbh.addTab(tbh.newTabSpec("TRA").setContent(R.id.tab7TransferenciaDeDatos).setIndicator("TRANSFERENCIA DE DATOS", null));
-
-        btnConvertirLongitud = findViewById(R.id.btnConvertirLongitud);
-        btnConvertirLongitud.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                spn = findViewById(R.id.spnDeLongitud);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnALongitud);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtCantidadLongitud);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = miObj.convertir(0, de, a, cantidad);
-                Toast.makeText(getApplicationContext(), "Respuesta: " + resp, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnConvertirAlmacenamiento = findViewById(R.id.btnConvertirAlmacenamiento);
-        btnConvertirAlmacenamiento.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                spn = findViewById(R.id.spnDeAlmacenamiento);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnAAlmacenamiento);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtCantidadAlmacenamiento);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = miObj.convertir(1, de, a, cantidad);
-                Toast.makeText(getApplicationContext(), "Respuesta: " + resp, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnConvertirMonedas = findViewById(R.id.btnConvertirMonedas);
-        btnConvertirMonedas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                spn = findViewById(R.id.spnDeMonedas);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnAMonedas);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtCantidadMonedas);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = miObj.convertir(2, de, a, cantidad);
-                Toast.makeText(getApplicationContext(), "Respuesta: " + resp, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnConvertirMasa = findViewById(R.id.btnConvertirMasa);
-        btnConvertirMasa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                spn = findViewById(R.id.spnDeMasa);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnAMasa);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtCantidadMasa);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = miObj.convertir(3, de, a, cantidad);
-                Toast.makeText(getApplicationContext(), "Respuesta: " + resp, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnConvertirVolumen = findViewById(R.id.btnConvertirVolumen);
-        btnConvertirVolumen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                spn = findViewById(R.id.spnDeVolumen);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnAVolumen);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtCantidadVolumen);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = miObj.convertir(4, de, a, cantidad);
-                Toast.makeText(getApplicationContext(), "Respuesta: " + resp, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnConvertirTiempo = findViewById(R.id.btnConvertirTiempo);
-        btnConvertirTiempo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                spn = findViewById(R.id.spnDeTiempo);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnATiempo);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtCantidadTiempo);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = miObj.convertir(5, de, a, cantidad);
-                Toast.makeText(getApplicationContext(), "Respuesta: " + resp, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnConvertirTransferenciaDeDatos = findViewById(R.id.btnConvertirTransferenciaDeDatos);
-        btnConvertirTransferenciaDeDatos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                spn = findViewById(R.id.spnDeTransferenciaDeDatos);
-                int de = spn.getSelectedItemPosition();
-
-                spn = findViewById(R.id.spnATransferenciaDeDatos);
-                int a = spn.getSelectedItemPosition();
-
-                tempVal = findViewById(R.id.txtCantidadTransferenciaDeDatos);
-                double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                double resp = miObj.convertir(6, de, a, cantidad);
-                Toast.makeText(getApplicationContext(), "Respuesta: " + resp, Toast.LENGTH_LONG).show();
-            }
-        });
-
+        tempVal = findViewById(R.id.lblSensorAcelerometro);
+        activarSensorAcelerometro();
     }
-}
+    @Override
+    protected void onResume() {
+        iniciar();
+        super.onResume();
+    }
 
-class conversores {
-    double[][] valores = {
-            {1, 100, 39.3701, 3.28084, 1.193, 1.09361, 0.001, 0.000621371},
-            {1, 0.001, 0.00000001, 0.000000001},
-            {1, 17.09, 833.99, 24.63},
-            {1, 1000, 1e+9, 1e+6, 35274, 2204.62},
-            {1, 1000, 0.264172, 1.05669},
-            {1, 60, 3600,  3600002.88},
-            {1, 1000, 1e+6},
-    };
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
 
-    public double convertir(int opcion, int de, int a, double cantidad){
-        return valores[opcion][a]/valores[opcion][de]*cantidad;
+    private void activarSensorAcelerometro(){
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor==null){
+            tempVal.setText("Tu dispositivo NO tiene el sensor de acelerometro");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                tempVal.setText("Acelerometro: X="+ sensorEvent.values[0] +"; Y="+ sensorEvent.values[1] +"; Z="+ sensorEvent.values[2]);
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+    private void iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
