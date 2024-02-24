5c995784bb0f3cf1ae98759542c291ffb59da83c
Jair <smtr139023@ugb.edu.sv>
2024-02-23 19:30:26 -0600

    Tarifa de agua potable por metro cuadrado y conversor de areas

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -4,57 +4,87 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TabHost;
 
 
+
+
 public class MainActivity extends AppCompatActivity {
-    TextView resultadoTextView;
-    EditText metrosConsumidosEditText;
     TabHost tbh;
-    TextView tempVal;
-    Button calcularButton;
+    TextView tempval;
+    Spinner spn;
+    Button btnArea;
+    Button btnCalcul;
+    conversores miObj = new conversores();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tbh = findViewById(R.id.tbhTarifa);
+        tbh = findViewById(R.id.tbhParcial);
         tbh.setup();
 
-        tbh.addTab(tbh.newTabSpec("TAR").setContent(R.id.tab1Tarifa).setIndicator("TARIFA", null));
+        tbh.addTab(tbh.newTabSpec("AGU").setContent(R.id.tabvalor_de_agua).setIndicator("Agua", null));
+        tbh.addTab(tbh.newTabSpec("ARE").setContent(R.id.tabArea).setIndicator("Area", null));
 
-        resultadoTextView = findViewById(R.id.resultado_text_view);
-        metrosConsumidosEditText = findViewById(R.id.metros_consumidos_edit_text);
-        calcularButton = findViewById(R.id.calcular_button);
-
-        calcularButton.setOnClickListener(new View.OnClickListener() {
+        btnArea = findViewById(R.id.btnConvertirArea);
+        btnArea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calcularValorPagar();
+                try {
+                    spn = findViewById(R.id.spnDEArea);
+                    int de = spn.getSelectedItemPosition();
+
+                    spn = findViewById(R.id.spnAArea);
+                    int a = spn.getSelectedItemPosition();
+
+                    tempval = findViewById(R.id.txtCantidadDeArea);
+                    double cantidad = Double.parseDouble(tempval.getText().toString());
+                    double resp = miObj.convertir(0, de, a, cantidad);
+                    Toast.makeText(getApplicationContext(), "Respuesta:" + resp, Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
             }
         });
-    }
+        btnCalcul = findViewById(R.id.btnCalcular);
+        btnCalcul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempval = findViewById(R.id.txtagua);
+                double agua = Double.parseDouble(tempval.getText().toString());
 
-    private void calcularValorPagar() {
-        String metrosConsumidosStr = metrosConsumidosEditText.getText().toString();
-        if (!metrosConsumidosStr.isEmpty()) {
-            int metrosConsumidos = Integer.parseInt(metrosConsumidosStr);
-            double valorPagar = 0;
+                double cal = 0;
+                if(agua<=18){
+                    cal=6;
+                }else if (agua<=28){
+                    cal = (agua-18)*0.45+6;
+                }else if(agua>28){
+                    cal= (agua-28)*0.65+((28-18)*0.45)+6;
+                }
 
-            if (metrosConsumidos >= 1 && metrosConsumidos <= 18) {
-                valorPagar = 6; // Cuota fija
-            } else if (metrosConsumidos >= 19 && metrosConsumidos <= 28) {
-                valorPagar = 6 + 0.45 * (metrosConsumidos - 18);
-            } else if (metrosConsumidos >= 29) {
-                valorPagar = 6 + 0.45 * 10 + 0.65 * (metrosConsumidos - 28);
+                tempval = findViewById(R.id.lblcal);
+                tempval.setText("Total a pagar: $"+cal);
             }
+        });
 
-            resultadoTextView.setText(String.format("Valor a pagar: $%.2f", valorPagar));
-        } else {
-            resultadoTextView.setText("Por favor ingresa la cantidad de metros consumidos.");
-        }
+
+
+    }
+}
+
+class conversores {
+    double[][] valores = {
+
+            {1, 1.4308, 1.19599, 10.7639, 0.0022896393817974, 0.0001431, 0.0001},
+    };
+
+    public double convertir(int opcion, int de, int a, double cantidad) {
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
